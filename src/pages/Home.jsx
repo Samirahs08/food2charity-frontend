12709184e3be4f629922d0aeb8d2947e7156eda0
samirahs08 <samirahs081529@gmail.com>
2026-03-10 10:21:40 +0530

    Added Home page module

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Food2Charity</h1>
+      <p>Reduce food waste and help people in need.</p>
+
+      <button>Donate Food</button>
+      <button>Request Food</button>
+    </div>
+  );
+}
+
+export default Home;
